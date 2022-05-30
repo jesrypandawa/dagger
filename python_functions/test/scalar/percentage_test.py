79c4ebb6f637bb5765863db4cf5a6317678651e1
jesrypandawa <jesry.p.pandawa@gmail.com>
2022-05-30 12:46:47 +0700

    test: test run

--- python_functions/test/scalar/percentage_test.py
+++
@@ -1,6 +0,0 @@
-from python_functions.udfs.scalar.percentage import percentage
-
-
-def testLog():
-    f = percentage._func
-    assert f(9876.5432) == 98.77
--- python_functions/udfs/scalar/percentage.py
+++
@@ -1,7 +0,0 @@
-from pyflink.table import DataTypes
-from pyflink.table.udf import udf
-
-
-@udf(result_type=DataTypes.FLOAT())
-def percentage(input_number):
-    return round((input_number/100), 2)
